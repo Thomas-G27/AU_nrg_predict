dadadc40b13e60ed36692b67952f072746e1c50a
Thomas-G27 <thomas.gbt2003@gmail.com>
2024-10-22 14:55:50 +0200

    set som pwr lvls and fixed a major calculation error line 30-ish

--- Software_of_printer/energy_estimation/get_energy.py
+++ Software_of_printer/energy_estimation/get_energy.py
@@ -1,15 +1,14 @@
 import csv
 
 # Set power levels
-POWER_ACCEL_X = 0.12*230
-POWER_ACCEL_Y = 0.16*230
-POWER_ACCEL_Z = 0.16*230
-POWER_SPEED_X = [0.1*230]
-POWER_SPEED_Y = [0.15*230]
-POWER_SPEED_Z = [0.15*230]
-POWER_HEAT_BED = 0.20*230
-POWER_HEAT_NOZ = 0.30*230
-POWER_PLUGGED = 0.15*230
+POWER_ACCEL_X = 0.12*230    #need update
+POWER_ACCEL_Y = 0.16*230    #need update
+POWER_ACCEL_Z = 0.16*230    #need update
+POWER_SPEED_X = [0.1*230]   #need update
+POWER_SPEED_Y = [0.15*230]  #need update
+POWER_SPEED_Z = [0.15*230]  #need update
+POWER_BED_NOZ = 0.431*230   #Confirmed experimental Value
+POWER_PLUGGED = 0.169*230   #Confirmed experimental Value
 POWER_BRAKE = 0.0
 
 
@@ -29,9 +28,8 @@ def calculer_energie(time, is_acc, speed_lvl, dx, dy, dz):
         pwr_speed = POWER_SPEED_Z[speed_lvl]
         if is_acc == 1:
             pwr_accel = POWER_ACCEL_Z
-    pwr = time * (POWER_PLUGGED + POWER_HEAT_BED + POWER_HEAT_NOZ + pwr_speed + pwr_accel)
-    energie = pwr * time
-    return energie
+    energie = time * (POWER_PLUGGED + POWER_BED_NOZ + pwr_speed + pwr_accel)
+    return energie 
 
 def add_nrg_comsumption(in_file, out_file):
     last_x = 0
@@ -40,31 +38,31 @@ def add_nrg_comsumption(in_file, out_file):
     last_t = 0
     with open(in_file, mode='r', newline='') as in_csvfile:
         lecteur_csv = csv.reader(in_csvfile)
-        en_tete = next(lecteur_csv)  # Lire l'en-tête
+        en_tete = next(lecteur_csv)  # Read head
         nrg_lines = []
 
-        # Ajouter un nouvel en-tête pour l'énergie consommée
+        # Add head columns
         head = en_tete[:4]
         head.append('J')
         head.append('I')
         nrg_lines.append(head)
 
-        # Lire chaque ligne et calculer l'énergie consommée
+        # Read each ligne and calculates energy/current consumption
         for line in lecteur_csv:
             x = float(line[0])
             y = float(line[1])
             z = float(line[2])
-            t = float(line[3])  # Temps écoulé
-            a = int(line[4])    # Accélération (peut être -1, 0, ou 1)
+            t = float(line[3])  # Total time
+            a = int(line[4])    # Acceleration (can be -1, 0, or 1)
             
-            #calcul des dist orthogonales
+            #calculate orthogonals distances
             dx = abs(x-last_x); dy = abs(y-last_y); dz = abs(z-last_z); dt = t-last_t
             last_x = x        ; last_y = y        ; last_z = z        ; last_t = t
             
-            # Calcul de l'énergie consommée pour cette ligne
+            # Calculate energie for csvline
             energie = calculer_energie(dt, a, 0, dx, dy, dz)
 
-            # Ajouter l'énergie à la ligne
+            # Add nrg and current to the line
             new_line = line[:4]
             new_line.append(energie)
             if dt !=0:
@@ -75,13 +73,12 @@ def add_nrg_comsumption(in_file, out_file):
             nrg_lines.append(new_line)
             
             
-    # Écrire les résultats dans un nouveau fichier CSV
+    # Write everything in a new csv file
     with open(out_file, mode='w', newline='') as out_csvfile:
         write_csv = csv.writer(out_csvfile)
         write_csv.writerows(nrg_lines)
 
-# Exemple d'utilisation du script
-#fichier_entree = 'coord_with_infos.csv'  # Remplacer par le nom de votre fichier CSV
-#fichier_sortie = 'coord_with_energy_consumption_test.csv'  # Le fichier CSV avec la consommation d'énergie
+# Example of script use ----------
+#fichier_entree = 'custom2.csv'  # Remplacer par le nom de votre fichier CSV
+#fichier_sortie = 'custom_energy2.csv'  # Le fichier CSV avec la consommation d'énergie
 #add_nrg_comsumption(fichier_entree, fichier_sortie)
-
